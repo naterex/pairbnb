8ef5a84b20ad5a0767df57da89726939efc29e1a
Nate Welling <nate@natewelling.com>
2016-02-22 13:46:38 +0800

    install Figaro gem, add first_name, last_name, password_confirmation to User model/forms

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'clearance'
 # bootstrap
 gem 'bootstrap-sass'
 
+# secure configure rails app
+gem "figaro"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,14 @@ class UsersController < Clearance::UsersController
       last_name = user_params.delete(:last_name)
       email = user_params.delete(:email)
       password = user_params.delete(:password)
+      password_confirmation = user_params.delete(:password_confirmation)
 
       Clearance.configuration.user_model.new(user_params).tap do |user|
         user.first_name = first_name
         user.last_name = last_name
         user.email = email
         user.password = password
+        user.password_confirmation = password_confirmation
       end
     end
 
